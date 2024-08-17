53c90bc41fd5cd98353b37842e4eeb5839773cce
Lavanya <lavanyawork21@gmail.com>
2024-08-17 11:02:32 +0530

    name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <div className="text-3xl bg-red-900">Heettyuiuiuuittttfffllo</div> 
+     <div className="text-3xl bg-red-900">lavanyagit</div> 
        
      
     </div>

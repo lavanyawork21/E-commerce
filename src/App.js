534bfab33b07627bdc4e5ff5a6d03826a4cd2c7d
Lavanya <lavanyawork21@gmail.com>
2024-08-17 10:45:32 +0530

    addfile

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="text-3xl bg-red-900">Heettyuiuiuuittttfffllo</div> 
+       
+     
+    </div>
+  );
+}
+
+export default App;
